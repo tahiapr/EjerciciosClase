58eacd953317c83df44dc0a43dee6758b69c9359
tperezrodriguez <tperezrodriguez@victoria5.danielcastelao.org>
2018-02-19 11:36:00 +0100

    Ejercicio de crear una factura de un comercio en el que se enlazan Arrays.

--- EjercicioComercio/src/ejerciciocomercio/EjercicioComercio.java
+++ EjercicioComercio/src/ejerciciocomercio/EjercicioComercio.java
@@ -17,21 +17,14 @@ public class EjercicioComercio {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        Ventas[] listaVentas = {new Ventas("1", "ref01", 12), new Ventas("2", "ref02", 8),
-            new Ventas("3", "ref03", 24), new Ventas("4", "ref04", 45)};
-
-        Producto[] listaProductos = {new Producto("ref04", "Mandarinas", "rp1"), new Producto("ref01", "Cerezas", "rp3"),
-            new Producto("ref02", "Sandías", "rp4"), new Producto("ref03", "Fresas", "rp2")};
-        
-        Precio[] listaPrecios = {new Precio("rp2", 4), new Precio("rp1", 9),
-            new Precio("rep4", 7), new Precio("rp3",5)};
-        
+       
         Metodos met = new Metodos();
 //        met.amosarPrecio(listaPrecios);
 //        met.amosarProductos(listaProductos);
 //        met.amosarVentas(listaVentas);
-        met.buscarReferenciaProducto(listaVentas, "ref04");
-     
+       
+     met.buscarReferencias();
+       
     }
 
 }
--- EjercicioComercio/src/ejerciciocomercio/Metodos.java
+++ EjercicioComercio/src/ejerciciocomercio/Metodos.java
@@ -13,26 +13,49 @@ import javax.swing.JOptionPane;
  */
 public class Metodos {
 
-    public void amosarVentas(Ventas[] listaVentas) {
+     Ventas[] listaVentas = {new Ventas("1", "ref01", 12), new Ventas("2", "ref02", 8),
+            new Ventas("3", "ref03", 24), new Ventas("4", "ref04", 45)};
+
+        Producto[] listaProductos = {new Producto("ref04", "Mandarinas", "rp1"), new Producto("ref01", "Cerezas", "rp3"),
+            new Producto("ref02", "Sandías", "rp4"), new Producto("ref03", "Fresas", "rp2")};
+        
+        Precio[] listaPrecios = {new Precio("rp2", 4), new Precio("rp1", 9),
+            new Precio("rp4", 7), new Precio("rp3",5)};
+        
+        
+    public void amosarVentas() {
         for (Ventas elemento : listaVentas) {
             System.out.println(elemento);
         }
     }
 
-    public void amosarProductos(Producto[] listaProductos) {
+    public void amosarProductos() {
         for (Producto elemento : listaProductos) {
             System.out.println(elemento);
         }
     }
 
-    public void amosarPrecio(Precio[] listaPrecios) {
+    public void amosarPrecio() {
         for (Precio elemento : listaPrecios) {
             System.out.println(elemento);
         }
     }
     
-    public Ventas buscarReferenciaProducto (Ventas[] listaVentas, String refProducto){
-  
-        return null;
+    public void buscarReferencias(){
+  for(int i=0; i<listaVentas.length;i++){
+      for(int j=0; j<listaProductos.length;j++){
+          for(int k=0; k<listaPrecios.length;k++){
+              if(listaVentas[i].getRefProducto().equalsIgnoreCase(listaProductos[j].getRefProducto())) {
+                  if(listaProductos[j].getRePrecio().equalsIgnoreCase(listaPrecios[k].getrPrecio())) {
+                      System.out.println("Número Ventas: "+listaVentas[i].getNv()+" || Nombre Producto: "+listaProductos[j].getNombre()+" || Precio Total: "+ listaVentas[j].getCantidade()*listaPrecios[k].getPrecio());  
+                  }
+              }
+          }
+      }
+          
+  }
+    
     }
+    
+    
 }
